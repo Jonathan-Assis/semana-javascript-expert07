f30d7e4d644491b3ffe3256cf525cf70250d1849
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-03-05 20:23:55 -0300

    Add gesture click :pinching_hand:

--- src/pages/titles/src/util/gestures.js
+++ src/pages/titles/src/util/gestures.js
@@ -2,6 +2,7 @@ const { GestureDescription, Finger, FingerCurl } = window.fp;
   
 const ScrollUpGesture = new GestureDescription('scroll-up'); // 🖐
 const ScrollDownGesture = new GestureDescription('scroll-down'); // ✊️
+const ClickGesture = new GestureDescription('click'); // 🤏
 
 
 // Scroll Up
@@ -11,9 +12,10 @@ const ScrollDownGesture = new GestureDescription('scroll-down'); // ✊️
 for(let finger of Finger.all) {
     ScrollUpGesture.addCurl(finger, FingerCurl.NoCurl, 1.0);
 }
+
 // Scroll Down
 // -----------------------------------------------------------------------------
-  
+
 // thumb: half curled
 // accept no curl with a bit lower confidence
 ScrollDownGesture.addCurl(Finger.Thumb, FingerCurl.HalfCurl, 1.0);
@@ -25,16 +27,35 @@ for(let finger of [Finger.Index, Finger.Middle, Finger.Ring, Finger.Pinky]) {
     ScrollDownGesture.addCurl(finger, FingerCurl.HalfCurl, 0.9);
 }
 
+// Click
+// -----------------------------------------------------------------------------
+
+ClickGesture.addCurl(Finger.index, FingerCurl.HalfCurl, 0.8)
+ClickGesture.addCurl(Finger.index, FingerCurl.FullCurl, 0.5)
+
+ClickGesture.addCurl(Finger.Thumb, FingerCurl.NoCurl, 1.0)
+ClickGesture.addCurl(Finger.Thumb, FingerCurl.HalfCurl, 0.4)
+
+ClickGesture.addCurl(Finger.Middle, FingerCurl.HalfCurl, 1.0)
+ClickGesture.addCurl(Finger.Middle, FingerCurl.FullCurl, 0.9)
+
+ClickGesture.addCurl(Finger.Ring, FingerCurl.HalfCurl, 1.0)
+ClickGesture.addCurl(Finger.Ring, FingerCurl.FullCurl, 0.9)
+
+ClickGesture.addCurl(Finger.Pinky, FingerCurl.HalfCurl, 1.0)
+ClickGesture.addCurl(Finger.Pinky, FingerCurl.FullCurl, 0.9)
 
 
 const knownGestures = [
     ScrollDownGesture,
     ScrollUpGesture,
+    ClickGesture
 ]
 
 const gestureStrings = {
     'scroll-up': '🖐',
     'scroll-down': '✊️',
+    'click': '🤏'
 }
 
 export {
