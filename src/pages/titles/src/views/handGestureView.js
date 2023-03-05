fca62c8c6eec2b9d2ebd6cc9b2fa3e7ed2a300d5
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-03-05 17:57:05 -0300

    Add scroll on view

---
+++ src/pages/titles/src/views/handGestureView.js
@@ -0,0 +1,12 @@
+export default class handGestureView {
+    loop(fn) {
+        requestAnimationFrame(fn)
+    }
+
+    scrollPage(top) {
+        scroll({
+            top,
+            behavior: 'smooth'
+        }) 
+    }
+}
